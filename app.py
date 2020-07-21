fddbf810872e28479c5847258c6d529882dd038c
Ben F <benfarniok@gmail.com>
2020-07-21 18:42:39 -0500

    github io stuff?

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import json
 # from bson.objectid import ObjectId
 
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
 
 
 # connect to your DB w/heroku or local
