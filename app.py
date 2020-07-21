985bba6194f5cc99659578603a17bf8126c7ceb1
Ben F <benfarniok@gmail.com>
2020-07-21 18:03:31 -0500

    heroku updates

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import json
 # from bson.objectid import ObjectId
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
 
 # connect to your DB w/heroku or local
 # app.config['MONGODB_URI'] = os.environ.get("MONGODB_URI") or "mongodb://localhost:27017/moviesDB2.movies"
